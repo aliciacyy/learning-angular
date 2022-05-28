7b7eb00cdb68771b095eead248bb59d97d4c6e32
aliciacyy <aliciacyy@gmail.com>
2022-05-28 16:17:33 +0800

    update before services

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('Test Recipe','This is a test','https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg?resize=1200:*'),
+    new Recipe('Test Recipe 2','This is a test','https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg?resize=1200:*')
+  ];
+
+  constructor() { }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
